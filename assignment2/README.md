43688888e0bba03dd3e473b1d77899deec78da68
Nicholas Karlsen <nicholas.karlsen@fys.uio.no>
2021-09-04 14:47:12 +0200

    worked on part 1 of assignment 2

---
+++ assignment2/README.md
@@ -0,0 +1,9 @@
+# Assignment 2
+
+## move.sh
+
+```bash
+./move.sh <source directory> <destination directory> <extension (optional)>
+```
+
+The extension argument lets you optionally only move files ending in i.e ".txt", ".dat".
---
+++ assignment2/move.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Ensure that the correct number of command-line arguments are supplied
+if [[ "$#" != [2-3] ]] 
+then
+    echo "[ERROR] Unexpected number of arguments!"
+    echo "------- expected usage: move \<source directory\> \<target directory\> <file extension (OPTIONAL)>"
+    exit 1
+fi
+
+src=$1
+dst=$2
+ext=${3:-*}
+
+# Check that both of the supplied directories exist.
+if [ ! -d $src ]
+then
+    echo [ERROR] $src does not exist in your filesystem!
+    exit 1
+fi
+
+# Check that the destination folder exists
+if [ ! -d $dst ]
+then
+    echo "Directory $dst does not exist in your filesystem."
+    read -p "--------- Do you wish to create this directory? (Y/N): " ans
+    case $ans in
+        [Yy]* ) echo You choose yes ;;
+        [Nn]* ) exit 1 ;;
+        * ) exit 1;; # Should exit regardless
+    esac
+
+    read -p "Do you with to append the time at the end of the new directory? (Y/N): " ans
+    case $ans in
+        [Yy]* ) dst+="_$(date '+%Y-%m-%d-%H-%M')" ;;
+        [Nn]* ) ;;
+        * ) ;;
+    esac
+
+    mkdir $dst
+fi
+
+mv $src/*$ext $dst
+
